ea1c7df85ca7addcdac47261b3eae505976ef495
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-03-30 13:45:25 +0530

    rk

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"zyntic-frontend",
+  base:"/zyntic-frontend",
   server: {
     port: 5173,    
   },
